58204c36d981a418492a31f25d240d78b1934571
Wongaboy <ajwong1001@gmail.com>
2023-10-18 02:54:08 -0700

    Added Game/Music/SFX Managers
    Added Empty objects with Manager Singleton Scripts

---
+++ Spades Capstone/Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    #region "Singleton"
+    private static MusicManager _instance;
+
+    public static MusicManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
