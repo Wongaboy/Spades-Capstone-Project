fdf7c82caf74d5475e5572469143ccb30dce4c85
Wongaboy <ajwong1001@gmail.com>
2023-10-23 13:58:55 -0700

    Added Score Manager Script
    Score Manager calculates end of round of results and keeps track of each players scores (Subject to change with implementation decisions)

---
+++ Spades Capstone/Assets/Scripts/Gameplay/ScoreManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    #region "Singleton"
+    private static ScoreManager _instance;
+
+    public static ScoreManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+
+    #region "Class Variables"
+
+    int player_Score = 0;
+    int player_Bonus = 0;
+
+    int death_Score = 0;
+    int death_Bonus = 0;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Function to calculate result of round (i.e Who Won?)
+    public void CheckRoundResult()
+    {
+        // Check Death's Played Card
+
+        // Check Player's Played Card
+
+        // Compare Cards
+
+        // Update Respective Scores Based on result
+        
+    }
+
+    public void CheckWin()
+    {
+        // Check if player_Score > 500
+
+        // Check if death_Score > 500
+    }
+}
