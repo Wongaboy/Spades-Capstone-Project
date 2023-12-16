f90c5f6810fcaa489a41d249c6139f44204c630a
snakelock <92760684+snakelock@users.noreply.github.com>
2023-12-15 18:23:18 -0800

    CARD DRAWING POGGERS

--- Spades Capstone/Assets/Scripts/Gameplay/Card.cs
+++ Spades Capstone/Assets/Scripts/Gameplay/Card.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 public class Card : MonoBehaviour
@@ -7,6 +8,7 @@ public class Card : MonoBehaviour
     [Header("Data")]
     public Suit suit;
     public int val;
+    [SerializeField] float cardSpeed = 10f;
 
     [Header("Components")]
     [SerializeField]
@@ -43,8 +45,26 @@ public class Card : MonoBehaviour
     // Will eventually use this to move cards into hands and on to tables and such - for the sake of JUICE
     public void MoveToLocation(Vector3 location, Quaternion rotation)
     {
-        gameObject.transform.position = location;
-        gameObject.transform.rotation = rotation;
+        //gameObject.transform.position = location;
+        //gameObject.transform.rotation = rotation;
+        StartCoroutine(TravelTo(location, rotation));
+    }
+
+    private IEnumerator TravelTo(Vector3 location, Quaternion rotation)
+    {
+        cardBody.detectCollisions = false;
+        Vector3 locInc = (location - gameObject.transform.position)/cardSpeed;
+        Quaternion rotQ = Quaternion.Inverse(gameObject.transform.rotation) * rotation;
+        Quaternion rotInc = Quaternion.Slerp(Quaternion.identity, rotQ, 1f / cardSpeed);
+        for (int i = 0; i< cardSpeed; i++)
+        {
+            yield return new WaitForSeconds(.05f);
+            gameObject.transform.position += locInc;
+            gameObject.transform.rotation *= rotInc;
+            
+        }
+        
+        cardBody.detectCollisions = true;
     }
 }
 
