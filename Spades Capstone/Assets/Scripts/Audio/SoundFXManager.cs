8466a2c18b1d9749170b13de146e3bbf8deabaa6
Wongaboy <ajwong1001@gmail.com>
2024-03-04 17:58:07 -0800

    Start of Sound Effects & Music Implementation

---
+++ Spades Capstone/Assets/Scripts/Audio/SoundFXManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    #region "Singleton"
+    private static SoundFXManager _instance;
+
+    public static SoundFXManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+
+    }
+    #endregion
+
+    #region "Class Variables"
+    // SoundFXObject Prefab reference
+    [SerializeField] private AudioSource soundFXObject;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //Spawn in GameObject
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        //Assign Audioclip
+        audioSource.clip = audioClip;
+
+        //Assign Volume
+        audioSource.volume = volume;
+
+        //Play Sound
+        audioSource.Play();
+
+        //Get Clip Length
+        float clipLength = audioSource.clip.length;
+
+        //Destroy Clip object after done playing
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
